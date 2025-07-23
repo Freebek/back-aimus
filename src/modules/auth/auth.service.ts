e41eec35f94548941360c0e4cc5aad2448c7859a
imronshoimov <imronshoimov125@gmail.com>
2025-07-23 14:52:05 +0500

    fixed bug

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -18,26 +18,35 @@ export class AuthService {
   async validateOrCreateSteamUser(profile: any): Promise<string> {
     const steamUser = profile._json;
 
-    const steamIdNum = BigInt(steamUser.steamid);
+    const user = await this.userModel.findOne({ steam_id: steamUser.steamid });
 
-    const Y = steamIdNum % 2n;
-    const Z = (steamIdNum - 76561197960265728n - Y) / 2n;
+    if (!user) {
+      const steamIdNum = BigInt(steamUser.steamid);
+      const Y = steamIdNum % 2n;
+      const Z = (steamIdNum - 76561197960265728n - Y) / 2n;
 
-    const user = await this.userModel.create({
-      steam_id: steamUser.steamid,
-      steam_id_64: steamUser.steamid,
-      steam_id_32: `STEAM_1:${Y}:${Z}`,
-      steam_id_3: `[U:1:${Z * 2n + Y}]`,
-      profile_url: steamUser.profileurl,
-      time_created: steamUser.timecreated,
-      country: steamUser.loccountrycode,
-      state: steamUser.locstatecode,
-      city: steamUser.loccityid,
-      steam_name: steamUser.personaname,
-      steam_avatar: steamUser.avatarfull,
-      is_steam_linked: true,
-      last_login_at: getCurrentTimeHelper(),
-    });
+      const user = await this.userModel.create({
+        steam_id: steamUser.steamid,
+        steam_id_64: steamUser.steamid,
+        steam_id_32: `STEAM_1:${Y}:${Z}`,
+        steam_id_3: `[U:1:${Z * 2n + Y}]`,
+        profile_url: steamUser.profileurl,
+        time_created: steamUser.timecreated,
+        country: steamUser.loccountrycode,
+        state: steamUser.locstatecode,
+        city: steamUser.loccityid,
+        steam_name: steamUser.personaname,
+        steam_avatar: steamUser.avatarfull,
+        is_steam_linked: true,
+        last_login_at: getCurrentTimeHelper(),
+      });
+
+      const token = this.jwtService.sign({
+        id: user._id,
+      });
+
+      return token;
+    }
 
     const token = this.jwtService.sign({
       id: user._id,
